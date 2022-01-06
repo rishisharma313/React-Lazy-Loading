927b3766a8c8260a0ccc7c881f56a509a05dce3f
rishisharma313 <rishisharma313@gmail.com>
2022-01-06 12:00:03 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, {Suspense} from 'react';
+import {Redirect, Route, Switch} from 'react-router-dom';
+
+import AllQuotes from './pages/AllQuotes';
+import QuoteDetail from './pages/QuoteDetail';
+import Layout from './components/layout/Layout';
+import LoadingSpinner from './components/UI/LoadingSpinner';
+
+const NewQuote = React.lazy(()=>import('./pages/NewQuote'));
+function App() {
+  return (
+    <div>
+      <Layout>
+        <Suspense fallback={
+          <div className='centered'>
+            <LoadingSpinner />
+          </div>
+        }>
+          <Switch>
+            <Route path="/" exact>
+              <Redirect to="/quotes" />
+            </Route>
+            <Route path="/quotes" exact>
+              <AllQuotes />
+            </Route>
+            <Route path="/quotes/:quoteId">
+              <QuoteDetail />
+            </Route>
+            <Route path="/new-quote">
+              <NewQuote />
+            </Route>
+          </Switch>
+        </Suspense>
+      </Layout>
+    </div>
+  );
+}
+
+export default App;
